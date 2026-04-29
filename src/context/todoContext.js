ddedf1898c2a62a6c0db8dbb4c529240cc295cde
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-04-30 00:52:28 +0500

    working on improved version

--- src/context/todoContext.js
+++ src/context/todoContext.js
@@ -1,18 +1,1 @@
-import { createContext, useContext } from "react";
 
-// create context
-export const ItemContext = createContext({
-  itemName: [],
-  setItemName: () => {},
-});
-
-// create custom hook for consumers to get data
-export const useItemName = () => {
-  const item = useContext(ItemContext);
-
-  if (!item) {
-    throw new Error("Item context not found...");
-  }
-
-  return item;
-};
