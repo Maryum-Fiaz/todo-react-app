f05528dc75a3c7660f31983721d5c921bbe6d6e5
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-04-30 15:03:39 +0500

    TodoContext & its definitions in App done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import { useState } from 'react';
+import { TodoContext } from './context/todoContext.js'
 
 function App() {
-  return (
-    <div>App</div>
-  )
+
+  const [ todos, setTodos ] = useState([])
+
+  // add item
+  const addTodos = (todo) => {
+    setTodos(prev => [{id: Date.now(), title: todo},...prev])
+  }
+
+  // Update item
+  const updateTodo = (id, todo) => {
+    setTodos(prev => 
+      prev.map(obj => 
+        obj.id === id ? {...obj, title: todo} : obj
+      )
+    )
+  }
+
+  // delete item
+  const deleteTodo = (id) => {
+    setTodos(prev => prev.filter(item => item.id !== id ))
+  }
+
+  // toggle to show todo completed
+  const toggleComplete = (id) => {
+    setTodos(prev =>
+      prev.map(obj => 
+        obj.id === id ? {...obj, completed: !obj.completed} : obj
+      )
+    )
+  }
+
+
+   return (
+    <TodoContext.Provider value={{todos, addTodos, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="flex flex-col justify-center items-center gap-2.5 h-screen bg-emerald-900">
+        <h1 className="font-bold text-2xl mb-2 text-amber-300">
+          Manage Your Todo
+        </h1>
+        <div className="bg-amber-100 p-3 flex flex-col justify-center items-center rounded-lg border-2 border-olive-800 w-2xl min-w-2xs">
+          <div className="mb-2">
+          <p>form here</p>
+
+          </div>
+
+          <div>
+          <p>items here</p>
+
+          </div>
+        </div>
+      </div>
+    </TodoContext.Provider>
+  );
 }
 
 export default App
--- src/context/todoContext.js
+++ src/context/todoContext.js
@@ -1,1 +1,24 @@
+import { createContext, useContext } from "react";
 
+// creating context
+export const TodoContext = createContext({
+    todos: [{
+        id: 1,
+        title: 'Todo msg',
+        completed: false
+    }],
+    addTodos: (todo) => {},  //won't define here
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+// custom hook
+export const useTodo = () => {
+    if(!TodoContext) {
+        throw new Error ('Context not found!')
+    }
+    
+    return useContext(TodoContext)
+}
